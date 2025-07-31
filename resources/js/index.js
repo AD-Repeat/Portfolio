926795ae0776115b66b2060d3f3630ebf5ea2103
TheRepeater77 <124745527+TheRepeater77@users.noreply.github.com>
2025-08-01 00:46:01 +0800

    Hero Animation JS to CSS

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,8 @@
 const root = document.documentElement;
 const computedStyles = getComputedStyle(root);
 
+const heroImg = document.querySelector("#HeroImg");
+
 let tick = 0;
 let seconds = 0;
 let secondsUpdate = 0;
@@ -12,34 +14,7 @@ let timeInterval = setInterval(() => {
     if(seconds > secondsUpdate){
         secondsUpdate = seconds;
     }
-    spotLightAnimation();
 }, 1);
-
-let spotLightY = 0;
-let spotLightEaseOut = 0;
-let spotLightMoveDone = false;
-
-let spotLightAlpha = 0;
-let spotLightAlphaEaseIn = 0;
-
-function spotLightAnimation(){
-    
-    if(spotLightEaseOut < 100){
-        spotLightEaseOut = spotLightEaseOut + 0.2;
-        spotLightY = easeOut(spotLightEaseOut/100) * 101;
-        spotLightY = Math.floor(spotLightY * 100)/100;
-        root.style.setProperty("--spotLightY",`calc(-50% + ${spotLightY}%)`);
-    } else {
-        spotLightMoveDone = true;
-    }
-
-    if(spotLightMoveDone == true && spotLightAlphaEaseIn < 100){
-        spotLightAlphaEaseIn = spotLightAlphaEaseIn + 0.5;
-        spotLightAlpha = easeOut(spotLightAlphaEaseIn/100);
-        spotLightAlpha = Math.floor(spotLightAlpha * 100)/100;
-        root.style.setProperty("--heroSpotLightAlpha",`${spotLightAlpha}`);
-    }
-}
 
 function easeOut(x){
     return Math.sin((x * Math.PI) / 2);
